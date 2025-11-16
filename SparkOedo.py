5eadfd74657d7ba31f03334d2c7d0abbe9bf6d04
Rin Yokoyama <yokoyama@cns.s.u-tokyo.ac.jp>
2025-11-16 18:52:41 +0900

    added environment.yaml

--- SparkOedo.py
+++ SparkOedo.py
@@ -2,6 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from hist.sparkHist1d import Hist1D, Hist1DArrays
 from hist.sparkHist2d import Hist2D, Hist2DArrays, Hist2DArrayVsPos
+from hist.fitHist1d import FitHist1DGauss
 from matplotlib import pyplot as plt
 from matplotlib.colors import LogNorm
 
@@ -13,6 +14,7 @@ __all__ = [
     "Hist2D",
     "Hist2DArrays",
     "Hist2DArrayVsPos",
+    "FitHist1DGauss",
     "plt",
     "LogNorm"
     ]
